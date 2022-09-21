84d99d3bf8215560f70096b91b3bdbb9811ce624
Jonathan Chen <jonchen228@gmail.com>
2022-09-21 02:18:47 -0400

    - add homepage description - absolute paths for working directory variable in shell scripts

--- statping.deploy.sh
+++ statping.deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WD=$(dirname $0)
+WD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SCRIPTS_DIR=${WD}/../../scripts
 export ENV_DIR=${WD}/../../../compose/conf/swarm-env-config
 export ENV_FILE=${ENV_DIR}/swarm.env
--- statping.remove.sh
+++ statping.remove.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WD=$(dirname $0)
+WD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SCRIPTS_DIR=${WD}/../../scripts
 export STACK_NAME=statping-swarm
 
